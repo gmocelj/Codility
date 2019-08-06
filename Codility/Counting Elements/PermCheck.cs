768111bc2cb3dc4e5971a9278158ad7663e9b1ee
Goran Mocelj <goran.mocelj@outlook.com>
2019-08-06 12:15:42 +0200

    Add project files.

---
+++ Codility/Counting Elements/PermCheck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Codility
+{
+    class PermCheck
+    {
+        public int Solution(int[] A)
+        {
+            Array.Sort(A);
+
+            for (var i = 0; i < A.Length; i++)
+            {
+                if (A[i] != i + 1)
+                {
+                    return 0;
+                }
+            }
+
+            return 1;
+        }
+    }
+}
---
+++ Codility/Iterations/BinaryGap.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Codility
+{
+    class BinaryGap
+    {
+        public int Solution(int N)
+        {
+            string binary = Convert.ToString(N, 2);
+
+            List<int> result = new List<int>();
+
+            int counter = 0;
+            foreach (char c in binary)
+            {
+                if (c == '1')
+                {
+                    if (counter != 0)
+                        result.Add(counter);
+                    counter = 0;
+                }
+                else
+                {
+                    counter++;
+                }
+            }
+
+            if (result.Count == 0)
+            {
+                return 0;
+            }
+
+            return result.Max();
+        }
+    }
+}
