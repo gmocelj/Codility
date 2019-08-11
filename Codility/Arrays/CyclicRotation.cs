d56971dc58736cba95c5bfb2205f0136e0a7e77b
Goran Mocelj <goran.mocelj@outlook.com>
2019-08-11 15:34:57 +0200

    Added algorithm for CyclicRotation

---
+++ Codility/Arrays/CyclicRotation.cs
@@ -0,0 +1,26 @@
+﻿namespace Codility.Arrays
+{
+    class CyclicRotation
+    {
+        public int[] solution(int[] A, int K)
+        {
+            if (A.Length == 0)
+            {
+                return A;
+            }
+
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            for (int numberOfTimes = 0; numberOfTimes < K; numberOfTimes++)
+            {
+                int firstPossition = A[A.Length - 1];
+                for (int i = A.Length - 1; i > 0; i--)
+                {
+                    A[i] = A[i - 1];
+                }
+                A[0] = firstPossition;
+            }
+
+            return A;
+        }
+    }
+}
