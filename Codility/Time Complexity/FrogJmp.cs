43aa27404672133af1a71faef6440381f3190534
Goran Mocelj <goran.mocelj@outlook.com>
2019-08-29 22:46:16 +0200

    FrogJump is implemented

---
+++ Codility/Time Complexity/FrogJmp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codility.Time_Complexity
+{
+    class FrogJmp
+    {
+        public int Solution(int X, int Y, int D)
+        {
+            decimal jumps = 0;
+            if (X >= Y)
+            {
+                return (int)jumps;
+            }
+
+            jumps = (decimal)(Y - X - D) / D;
+            jumps++; // because we removed first step in line above
+
+            if (jumps % 1 != 0)
+                jumps++;
+
+            return (int)jumps;
+        }
+    }
+}
