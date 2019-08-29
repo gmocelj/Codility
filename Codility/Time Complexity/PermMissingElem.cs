8b492ab6f12d7bb06e4d225f93baea474fae272e
Goran Mocelj <goran.mocelj@outlook.com>
2019-08-29 23:23:40 +0200

    Implemented PermMissingElem

---
+++ Codility/Time Complexity/PermMissingElem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codility.Time_Complexity
+{
+    public class PermMissingElem
+    {
+        public int solution(int[] A)
+        {
+            if (A.Length == 0)
+                return 1;
+
+            if (A.Length == 1)
+            {
+                if (A[0] > 1)
+                    return 1;
+                else
+                    return A[0] + 1;
+            }
+
+            Array.Sort(A);
+            if (A[A.Length - 1] == A.Length)
+            {
+                return A.Length + 1;
+            }
+
+            for (int index = 0; index < A.Length - 1; index++)
+            {
+                if (A[index] + 1 != A[index + 1])
+                {
+                    return A[index] + 1;
+                }
+            }
+
+            return 1;
+        }
+    }
+}
