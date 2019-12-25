3eeaaa96090aa4db29eb16fa2f89bb3a925c2e9b
Goran Mocelj <goran.mocelj@outlook.com>
2019-12-25 12:55:46 +0100

    In progress TapeEquilibrium

---
+++ Codility/Time Complexity/TapeEquilibrium.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Codility.Time_Complexity
+{
+    public class TapeEquilibrium
+    {
+        public int solution(int[] A)
+        {
+            List<int> results = new List<int>();
+            for (int i = 0; i < A.Length - 2; i++)
+            {
+                results.Add(CalculateParts(i + 1, A));
+            }
+
+            return results.Min();
+        }
+
+        private int CalculateParts(int secondBegin, int[] A)
+        {
+            int firstResult = 0;
+            int secondResult = 0;
+
+            for (int i = 0; i < secondBegin; i++)
+            {
+                firstResult += A[i];
+            }
+            for (int y = secondBegin; y < A.Length; y++)
+            {
+                secondResult += A[y];
+            }
+
+            return Math.Abs(firstResult - secondResult);
+        }
+    }
+}
