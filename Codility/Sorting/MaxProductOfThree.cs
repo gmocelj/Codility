4140ac4cdc2861ff7aa45ada214cdc09c3bb309b
goranmocelj <109068865+goranmocelj@users.noreply.github.com>
2024-12-25 16:27:45 +0100

    MaxProductOfThree finished

--- Codility/Sorting/MaxProductOfThree.cs
+++ Codility/Sorting/MaxProductOfThree.cs
@@ -1,18 +1,26 @@
 using System;
-using System.Linq;
 
 namespace Codility
 {
     public class MaxProductOfThree
     {
-        public int solution(int[] A) => getMaxValue(A) * getMaxValue(A) * getMaxValue(A);
-
-        private int getMaxValue(int[] A)
+        public int solution(int[] A)
         {
-            var max = A.Max(b=> b);
-            var maxIndex = Array.IndexOf(A, max);
-            A[maxIndex] = Int32.MinValue;
-            return max == 0 ? getMaxValue(A) : max;
+            if (A.Length < 3)
+            {
+                return 0;
+            }
+            
+            Array.Sort(A);
+
+     
+            int n = A.Length;
+            int maxProduct = Math.Max(
+                A[n - 1] * A[n - 2] * A[n - 3],
+                A[0] * A[1] * A[n - 1]
+            );
+
+            return maxProduct;
         }
     }
 }
