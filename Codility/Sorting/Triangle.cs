06fe39bf9c3c560c25056e7c248695ec076d35c8
goranmocelj <109068865+goranmocelj@users.noreply.github.com>
2025-03-05 19:48:11 +0100

    Triangle solution update

--- Codility/Sorting/Triangle.cs
+++ Codility/Sorting/Triangle.cs
@@ -8,21 +8,17 @@ public class Triangle
             return 0;
         }
 
-        if (A.Length == 3 && A[0] == int.MaxValue && A[1] == int.MaxValue && A[2] == int.MaxValue)
-        {
-            return 1;
-        }
         int step = A.Length / 3;
         for(int i = 0; i<A.Length-2 * step; i++)
         {
-            if(A[i] + A[i+step]>A[i+ 2 * step] &&
-               A[i+step] + A[i + 2 * step]> A[i] &&
-               A[i+ 2 * step] + A[i]> A[i+step])
+            if((long)A[i] + A[i+step]>A[i+ 2 * step] &&
+               (long)A[i+step] + A[i + 2 * step]> A[i] &&
+               (long)A[i+ 2 * step] + A[i]> A[i+step])
             {
                 return 1;
             }
         }
         
-        return 0;    
+        return 0;   
     }
 }
